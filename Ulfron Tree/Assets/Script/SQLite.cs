831a5910825a9a87af73aace588d17eb0a40e553
RyuTatsukiSama <dorianribault@gmail.com>
2025-01-28 10:22:31 +0100

    Add SQLite and start test

---
+++ Ulfron Tree/Assets/Script/SQLite.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+
+public class SQLite : MonoBehaviour
+{
+    string conn;
+    string sqlQuery;
+    IDbConnection dbconn;
+    IDbCommand dbcmd;
+
+    string DATABASE_NAME = "/mydatabase.s3db";
+
+    void Start()
+    {
+        string filepath = Application.dataPath + DATABASE_NAME;
+        conn = "URI=file:" + filepath;
+        //dbconn = new SQLiteConnection(conn);
+        CreateTable();
+    }
+
+    private void CreateTable()
+    {
+        //using (dbconn = new SQLiteConnection(conn))
+        {
+            dbconn.Open();
+            dbcmd = dbconn.CreateCommand();
+
+            sqlQuery = "CREATE TABLE IF NOT EXISTS [table_name] (" +
+                "[id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "[name] VARCHAR(255)  NOT NULL," +
+                "[age] INTEGER DEFAULT '18' NOT NULL)";
+
+            dbcmd.CommandText = sqlQuery;
+            dbcmd.ExecuteScalar();
+            dbconn.Close();
+        }
+
+    }
+}
